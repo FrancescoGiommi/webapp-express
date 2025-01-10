9a7bf9106f44b34f240682f139424ec887110fec
FrancescoGiommi <francescogiommi97@icloud.com>
2025-01-10 19:11:48 +0100

    Add reviews to show query

--- controller/moviesController.js
+++ controller/moviesController.js
@@ -5,9 +5,11 @@ function index(req, res) {
   const sql = "SELECT id,title,director,genre,image FROM `movies`";
   connection.query(sql, (err, results) => {
     if (err) return res.status(500).json({ error: "Database query failed" });
+
+    const [movie] = results;
     res.json({
       status: "OK",
-      movies: results,
+      movies: movie,
     });
   });
 }
@@ -15,13 +17,28 @@ function index(req, res) {
 /* Show  */
 function show(req, res) {
   const movieId = req.params.id;
-  const showSql =
-    "SELECT id,title,director,genre,image FROM `movies` WHERE id = ?";
-  connection.query(showSql, [movieId], (err, results) => {
-    if (err) return res.status(500).json({ error: "Database query failed" });
-    res.json({
-      message: "ok",
-      movie: results[0],
+  const sqlMovie =
+    "SELECT id,title,director,genre,release_year,abstract,image FROM `movies` WHERE id = ?";
+
+  connection.query(sqlMovie, [movieId], (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: "Database query failed" });
+    }
+    const [movie] = results;
+    const sqlReviews =
+      "SELECT id,name,vote,text FROM reviews WHERE movie_id = ?";
+    connection.query(sqlReviews, [movieId], (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ error: "Database query failed" });
+      }
+
+      movie.reviews = results;
+      res.json({
+        status: "OK",
+        movie,
+      });
     });
   });
 }
